762eec6a4dd51686e17b200a13036f118403a6b3
x-color <36035885+x-color@users.noreply.github.com>
2019-06-08 10:47:37 +0900

    Add evaluator to REPL (chapter 3.5.2)

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/x-color/monkey/lexer"
 	"github.com/x-color/monkey/parser"
+	"github.com/x-color/monkey/evaluator"
 )
 
 const prompt = ">> "
@@ -29,8 +30,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
